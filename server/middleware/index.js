e329677260277e0e335e57935c73e1194dada4cf
DennisLi755 <djl6347@g.rit.edu>
2024-04-03 12:24:27 -0400

    fixed heroku issues (please)

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -13,8 +13,8 @@ const requiresLogout = (req, res, next) => {
 };
 
 const requiresSecure = (req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'http') {
-    return res.redirect(`http://${req.hostname}${req.url}`);
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(`https://${req.hostname}${req.url}`);
   }
   return next();
 };
